5693fd044ce9f193937d2d4097e7bd1079907afb
Ludmila <lipatovalv19@yandex.ru>
2023-04-16 23:06:32 +0400

    написала программу для фильтрации

--- test2.py
+++ test2.py
@@ -8,3 +8,16 @@ def filter_strings(input_array, max_length):
             result_array.append(string)
     return result_array
 
+# Вводим исходный массив строк с клавиатуры
+input_array = input("Введите строки через пробел: ").split()
+
+# Задаем максимальную длину строки
+max_length = 3
+
+# Формируем новый массив строк, длина которых меньше либо равна 3 символам
+result_array = filter_strings(input_array, max_length)
+
+# Выводим результат
+print("Результат:")
+for string in result_array:
+    print(string)
